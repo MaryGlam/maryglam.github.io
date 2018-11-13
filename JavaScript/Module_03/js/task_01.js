a6efc7dbba11ca269a302d87a5d7b945e51827b5
MaryGlam <mglamazda@yahoo.com>
2018-11-13 22:16:24 +0200

    Module 03: comments cleared

--- JavaScript/Module_03/js/task_01.js
+++ JavaScript/Module_03/js/task_01.js
@@ -2,31 +2,26 @@
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 const msgError = "Ошибка! Логин должен быть от 4 до 16 символов";
 const msgAdded = "Логин успешно добавлен!";
+const login = prompt("Введите, пожалуйста, login");
 
-let login = prompt("Введите, пожалуйста, login");
 let loginLenght;
 let loginIncludes;
 let allLogins;
 
-const isLoginValid = function(login) {
-  return (loginLenght = login.length <= 16 && login.length >= 4);
+const isLoginValid = (login) => {
+  return login.length <= 16 && login.length >= 4;
 };
 
-const isLoginUnique = function(allLogins, login) {
-  allLogins = logins;
-  return (loginIncludes = allLogins.includes(login));
+const isLoginUnique = (logins, login) => { 
+  return (logins.includes(login));
 };
 
-const addLogin = function(logins, login) {
-  isLoginValid(login);
-  isLoginUnique(allLogins, login);
-
-  if (!loginLenght) {
+const addLogin = (logins, login) => {
+  if (!isLoginValid(login)) {
     alert(msgError);
-  } else if (!loginIncludes) {
-    allLogins = logins;
-    logins = allLogins.push(login);
-    alert("Логин успешно добавлен!");
+  } else if (!isLoginUnique(logins,login)) {
+    logins.push(login);
+    alert(msgAdded);
   } else alert("Такой логин уже используется!");
 };
 
